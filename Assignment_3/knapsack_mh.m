5ac20964191ebea2dbeced99dc3183c73047323f
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-11-10 19:30:07 +0100

    Update knapsack_mh.m

--- Assignment_3/knapsack_mh.m
+++ Assignment_3/knapsack_mh.m
@@ -6,11 +6,47 @@ function [publ_k, priv_k] = knapsack_mh(s)
     
     valide = knapsack(s);
     s_length = length(s);
+    sum_of_s = sum(s);
     
     % Checks if s is a valid knapsack.
     if valide == -1
         error('The input is not a knapsack.');
         return;
+    end
+    
+    prompt = sprintf('Introduce the value mu, a natural number greater than %s:\n> ', string(sum_of_s));
+    mu = input(prompt);
+    
+    % Random value generator.
+    rng(0, 'twister');
+    
+    % Check mu constraint.
+    if mu <= sum_of_s
+        mu = randi([sum_of_s, sum_of_s * 256], 1, 1);
+        warning('The value of mu was not valid. It was stablished to %d.', mu);
+    end
+    
+    priv_k = [];
+    priv_k(end + 1) = mu;
+    
+    flag = 1;
+    while flag == 1
+        % Existing x in range [2, mu - 2].
+        x = randi([2, mu - 2], 1, 1);
+        w = x / gcd(mu, x);
+        
+        if gcd(w, mu) == 1
+            [flag, factor] = common_factors(w, s);
+        end
+    end
+    
+    %TODO: 
+    %   - Falta introducir en priv_k el inverso w mod(mu).
+    priv_k(end + 1) = -1;
+    
+    publ_k = [];
+    for i = 1:s_length
+        publ_k(end + 1) = mod( (w * s(i) ), mu);
     end
     
     toc

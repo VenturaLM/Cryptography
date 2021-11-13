f1965b1f38c2ac78345e1950fef98fa6356dc663
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-11-13 17:56:15 +0100

    Assignment finished!

--- Assignment_3/crypt_shamir_zippel.m
+++ Assignment_3/crypt_shamir_zippel.m
@@ -5,33 +5,42 @@ function s = crypt_shamir_zippel(publ_k, mu)
     tic
     
     publ_k_size = length(publ_k);
-    [G, U, V] = gcd(publ_k(2), mu);
+    [~, U, ~] = gcd(publ_k(2), mu);
     b2_inv = mod(U, mu);
     q = mod(publ_k(1)*b2_inv, mu);
     
-    q_array = [2^(publ_k_size + 1)];
-    
     counter = 0;
+    lower = 1;
+    
     while 1
         
         % Find { q, 2q, ..., 2^(n+1) } mod (mu).
         range = 2^(publ_k_size + 1) * 2^counter;
-        for i = 2^(publ_k_size + 1) * counter + 1:range
-            q_array(end + 1) = mod((2^(publ_k_size + 1) * counter) + i * q, mu);
-        end
+        q_array = [range];
 
+        for i = lower:range
+            q_array(end + 1) = mod(i * q, mu);
+        end
+        
         % Sort { q, 2q, ..., 2^(n+1) } mod (mu).
         q_array = sort(q_array);
 
-        % Take the first candidate a_{1}.
-        [G, U, V] = gcd(q_array(1), mu);
-        w = mod(publ_k(1) * mod(U, mu), mu);
+        % Take the first candidate a_{1} from q_array.
+        q_array_size = length(q_array);
+
+        for i = 1:q_array_size
+            [~, U, ~] = gcd(q_array(i), mu);
+            w = mod(publ_k(1) * mod(U, mu), mu);
+            
+            if w ~= 0
+                break;
+            end
+        end
 
         % Find multiplicative factor w.
-        % TODO: w no tiene inverso a partir de una iteracion.
-        [G, U, V] = gcd(w, mu);
+        [~, U, ~] = gcd(w, mu);
         w_inv = mod(U, mu);
-
+    
         % Potential simple knapsack.
         priv_k = [];
         priv_k(end + 1) = q_array(1);
@@ -41,22 +50,25 @@ function s = crypt_shamir_zippel(publ_k, mu)
 
         s = priv_k;
         valide = knapsack(s);
-disp(s);
+
         % Checks if s is a valid knapsack.
+        fprintf('Searched multiples in the rank [%d, %d].\n\n', lower, range);
+        disp('-----------------------------------------------------');
         if valide == -1 || valide == 0
             warning('The input is not a simple knapsack.\n');
-            %selection = input('Do you want to continue with a bigger interval? (Y/N): ', 's');
-            selection = 1;
+            selection = input('Do you want to continue with a bigger interval? (Y/N): ', 's');
+            selection = upper(selection);
+            
             if selection == 'N'
                 s = -1;
                 return
             end
-            fprintf('Searched multiples in the rank [%d, %d].\n\n', 2^(publ_k_size + 1) * counter + 1, range);
         else
             return
         end
         
         counter = counter + 1;
+        lower = range;
     end
     
     toc

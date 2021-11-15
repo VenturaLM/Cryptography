c8d33efc4ec5165b15ff2cca5f27e33b20e78b07
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-11-15 15:54:23 +0100

    Update power_mod.m

--- Assignment_4/power_mod.m
+++ Assignment_4/power_mod.m
@@ -25,11 +25,11 @@ function pow = power_mod(b, e, m)
         end
     end
 
-    % Computer the powers from [ b^e^powers_0 , b^e^powers_n ].
+    % Compute the powers from [ b^e^powers_0 , b^e^powers_n ].
     result = [];
     last_power = powers(end);
     % Line 32: Actually mod(b^(2^(0)), m), what is equal to mod(b, m).
-    result(end + 1) = mod(b, m);
+    result(end + 1) = uint64(mod(b, m));
 
     for i = 1:last_power
         new_power = result(end) * result(end);
@@ -39,7 +39,7 @@ function pow = power_mod(b, e, m)
     powers_length = length(powers);
     % Since MATLAB's first element is 1, add to the first element + 1, so that 0 --> 1.
     powers = powers + 1;
-    pow = 1;
+    pow = uint64(1);
     for i = 1:powers_length
         pow = mod( uint64(result(powers(i))) * uint64(pow), m );
     end

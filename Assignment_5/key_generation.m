e84149563e65dda25091dc88a77ac656e2d84d9b
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-11-30 15:53:26 +0100

    Assignment 5 finished.

--- Assignment_5/key_generation.m
+++ Assignment_5/key_generation.m
@@ -1,39 +1,40 @@
-%KEY_GENERATION Program that generates both private and public keys
-%needed in RSA cipher.
-%   - p and q values are required by user.
+function [n, e, d] = key_generation()    
+    %KEY_GENERATION Function that generates both private and public keys
+    %needed in RSA cipher.
+    %   - p and q values are required by user.
 
-% Choose 2 prime numbers.
-p = input('Introduce the prime number p:\n> ');
-assert(~isprime(p) == 0, '%d is not a primer number.\n', p);
+    % Choose 2 prime numbers.
+    p = input('Introduce the prime number p:\n> ');
+    assert(~isprime(p) == 0, '%d is not a primer number.\n', p);
 
-q = input('Introduce the prime number q:\n> ');
-assert(~isprime(q) == 0, '%d is not a primer number.\n', q);
+    q = input('Introduce the prime number q:\n> ');
+    assert(~isprime(q) == 0, '%d is not a primer number.\n', q);
 
-% Calculate n = p · q.
-n = p * q;
-fprintf('\nThe value n is n = p · q = %d\n\n', n);
+    % Calculate n = p · q.
+    n = p * q;
+    fprintf('\nThe value n is n = p · q = %d\n\n', n);
 
-% Choose e < phi(n).
-%   Notes: 
-%   - phi(n) = (p - 1) · (q - 1).
-%   - gcd(e, phi(n)) = 1
-phi_of_n = (p - 1) * (q - 1);
-e = 65537; % e = Fermat_4 = 1 + 2^(2^4).
+    % Choose e < phi(n).
+    %   Notes: 
+    %   - phi(n) = (p - 1) · (q - 1).
+    %   - gcd(e, phi(n)) = 1
+    phi_of_n = (p - 1) * (q - 1);
+    e = 65537; % e = Fermat_4 = 1 + 2^(2^4).
 
-assert(e < phi_of_n, 'Constraint [e < phi(n)] not satisfied.');
-assert(gcd(e, phi_of_n) == 1, 'Constraint gcd(e, phi_of_n) ~= 1');
+    assert(e < phi_of_n, 'Constraint [e < phi(n)] not satisfied.');
+    assert(gcd(e, phi_of_n) == 1, 'Constraint gcd(e, phi_of_n) ~= 1');
 
-fprintf('The value e, of the public key, should satisfy gcd(e, phi(n)) = gcd(e, (p - 1)·(q - 1)) = gcd(e, %d) = 1.\n', phi_of_n);
-fprintf('It is selected e = %d.\n\n', e);
+    fprintf('The value e, of the public key, should satisfy gcd(e, phi(n)) = gcd(e, (p - 1)·(q - 1)) = gcd(e, %d) = 1.\n', phi_of_n);
+    fprintf('It is selected e = %d.\n\n', e);
 
-% Find d such 1 < d < phi(n), such d = inv(e) mod phi(n).
-[~, U, ~] = gcd(e, phi_of_n);
-d = mod(U, phi_of_n);
+    % Find d such 1 < d < phi(n), such d = inv(e) mod phi(n).
+    [~, U, ~] = gcd(e, phi_of_n);
+    d = mod(U, phi_of_n);
 
-assert(d > 1, 'd is lower than 1.');
-assert(d < phi_of_n, 'd is greater that phi(n).');
+    assert(d > 1, 'd is lower than 1.');
+    assert(d < phi_of_n, 'd is greater that phi(n).');
 
-fprintf('d is the inverse of e module phi(n): d = %d.\n\n', d);
+    fprintf('d is the inverse of e module phi(n): d = %d.\n\n', d);
 
-fprintf('PRIVATE KEY: (n, d) = (%d, %d).\n', n, d);
-fprintf('PUBLIC KEY: (n, e) = (%d, %d).\n', n, e);
+    fprintf('PRIVATE KEY: (n, d) = (%d, %d).\n', n, d);
+    fprintf('PUBLIC KEY: (n, e) = (%d, %d).\n', n, e);
--- Assignment_5/sign_auth.m
+++ Assignment_5/sign_auth.m
@@ -1,27 +1,16 @@
 %SIGN_AUTH Program that includes all the necessary steps to encrypt
 %and decrypt a message using RSA method with signature authentication.
 
-% USE KEY GENERATION SCRIPT HERE!
+% Calculate the public and private key of A.
+fprintf('AGENT A\n');
+[na, ea, da] = key_generation();
 
-% Calculate the public key of A.
-na = 151535011;
-ea = 19;
+% Calculate the public and private key of B.
+fprintf('\nAGENT B\n');
+[nb, eb, db] = key_generation();
 
-% Calculate the private key of A.
-da = 47845387;
-
-% Calculate the public key of B.
-nb = 1389977;
-eb = 179;
-
-% Calculate the private key of B.
-db = 1271339;
-
-
-%text_a = input('Introduce the text you want to send to B:\n> ');
-%signature_a = input('Introduce your signature:\n> ');
-text_a = 'buenos dias, andrea';
-signature_a = 'andrea';
+text_a = input('\nIntroduce the text you want to send to B:\n> ', 's');
+signature_a = input('Introduce your signature:\n> ', 's');
 
 % A ciphers the text with B's public key.
 ciphered_text_a = rsa_cipher(nb, eb, text_a);
@@ -64,6 +53,12 @@ signature = rsa_num_decipher(nb, db, ciphered_signature_a);
 char_signature = '';
 for i = 1:length(signature)
     char_signature = strcat(char_signature, num2str(signature(i)));
+end
+
+% Check for 30's at the end.
+while char_signature(end - 1) == '3' && char_signature(end) == '0'
+    char_signature(end) = [];
+    char_signature(end) = [];
 end
 
 % Prepare decipher blocks.

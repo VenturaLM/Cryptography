291056d589a4321fd20a3fc5b978c0e7f1159cd9
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-12-11 18:52:21 +0100

    Update arnold_02.m

--- Assignment_6/arnold_02.m
+++ Assignment_6/arnold_02.m
@@ -12,26 +12,25 @@ function power = arnold_02(photo, A)
     %   - case 1: disorder a photo until reach the original one.
     %   - case 2: disorder a photo n times. n is selected by the user.
     
-    power = -1;
-    % NOTA: Aunque no sea un parámetro de salida, también debe mostrar una animación que comience
-    %con la imagen original y muestre todas las imágenes transformadas que se hayan ido realizando.
-    %También debe guardar la última imagen de la sucesión de imágenes modificadas.
-
-    
     option = input('Options:\n1. Disorder until getting the original image.\n2. Apply a certain number of transformations.\n3. Exit.\n> ', 's');
     
     % Flag used for an infinite loop until a valid option is selected.
     flag = 1;
-
+    
+    module = imfinfo(photo).Width;
+    assert(module > 0, 'The work module is not valid. Check if <photo> is a valid input.');
+    
     while flag == 1
         switch option
             case '1'
-                
+                % Disorder until reach the original image.
+                power = power2(A, module);
                 
                 flag = 0;
                 
             case '2'
-                
+                % Disorder n times.
+                power = str2num(input('How many transformations do you want to apply?\n> ', 's'));
                 
                 flag = 0;
                 
@@ -43,5 +42,20 @@ function power = arnold_02(photo, A)
                 option = input('Options:\n1. Disorder until getting the original image.\n2. Apply a certain number of transformations.\n3. Exit.\n> ', 's');
         end
     end
+    
+    % Apply transformation.
+    A_power = A;
+    
+    for i = 1:power
+        A_power = mod(A * A_power, module);
+    end
+    
+    pixel_disorder(photo, A_power);
+    imshow(getappdata(gcf, 'matrix'));
+    
+    % TODO: Aunque no sea un parámetro de salida, también debe mostrar una animación que comience
+    %con la imagen original y muestre todas las imágenes transformadas que se hayan ido realizando.
+    %También debe guardar la última imagen de la sucesión de imágenes modificadas.
+    
 end
 
